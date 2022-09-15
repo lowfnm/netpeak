083c7bb1fb2c7dc535b99bbd5659d1e5f0e17ec4
lowfnm <artem.melinevskiy21@gmail.com>
2022-09-15 04:47:18 +0300

    🏀 Made monthly/yearly script

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-const nav = () => {
+const main = () => {
   const nav = document.querySelector('.js-nav');
   const navLinks = nav.querySelectorAll('.nav__link');
   const slideRect = nav.querySelector('.nav__slider-rect');
+  const starterTab = document.getElementById('starter');
+  const professionalTab = document.getElementById('professional');
+  const companyTab = document.getElementById('company');
 
   nav.addEventListener('click', (evt) => {
     if (!evt.target.classList.contains('nav__link')) return;
@@ -14,7 +17,17 @@ const nav = () => {
     }
 
     slideRect.style.transform = `translateX(${evt.target.dataset.transform}%)`;
+
+    if (slideRect.style.transform === 'translateX(100%)') {
+      starterTab.innerText = '$12';
+      professionalTab.innerText = '$36';
+      companyTab.innerText = '$56';
+    } else {
+      starterTab.innerText = '$19';
+      professionalTab.innerText = '$56';
+      companyTab.innerText = '$89';
+    }
   });
 };
 
-nav();
+main();
